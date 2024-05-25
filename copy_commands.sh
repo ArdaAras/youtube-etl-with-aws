7cc469a3cd1b71b6ad8866591d1426e237340d80
Arda <arda.aras.dev@gmail.com>
2024-05-25 20:22:03 +0300

    Update copy_commands.sh
    Uncomment and minor typo fix

--- copy_commands.sh
+++ copy_commands.sh
@@ -2,10 +2,7 @@
 aws s3 cp . s3://raw-youtube-data-eucentral1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
 
 # To copy all data files to its own location, following Hive-style patterns:
-<<com
-aws s3 cp CAvideos.csv s3://raw-youtube-data-eucentral1-de/youtube/raw_statistics/region=ca/
-com
-
+aws s3 cp CAvideos.csv s3://raw-youtube-data-eucentral1-dev/youtube/raw_statistics/region=ca/
 aws s3 cp DEvideos.csv s3://raw-youtube-data-eucentral1-dev/youtube/raw_statistics/region=de/
 aws s3 cp FRvideos.csv s3://raw-youtube-data-eucentral1-dev/youtube/raw_statistics/region=fr/
 aws s3 cp GBvideos.csv s3://raw-youtube-data-eucentral1-dev/youtube/raw_statistics/region=gb/
